2bf418949ed0b435cf1225aa88e3960b5fe56fb5
Raymond Jacobson <me@raymondjacobson.com>
2018-09-08 19:12:13 -0700

    add redux and basic routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
+import About from './about';
+import Feed from './feed';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <Link to="/">Feed</Link>
+          <Link to="/about">About</Link>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <main>
+          <Route exact path="/" component={Feed} />
+          <Route exact path="/about" component={About} />
+        </main>
       </div>
     );
   }
---
+++ src/feed/feed.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './feed.css';
+
+class Feed extends Component {
+  render() {
+    return <p>feed.</p>;
+  }
+}
+
+export default Feed;
